782f31160a386aa461955650e53a19bb09386aa8
trisct <29689546+trisct@users.noreply.github.com>
2021-03-15 16:22:31 +0800

    new file:   unittest_deep_sdf.py

---
+++ unittest_deep_sdf.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+# Copyright 2004-present Facebook. All Rights Reserved.
+
+import torch
+import torch.utils.data as data_utils
+import signal
+import sys
+import os
+import logging
+import math
+import json
+import time
+
+import deep_sdf
+import deep_sdf.workspace as ws
+
+
+class LearningRateSchedule:
+    def get_learning_rate(self, epoch):
+        pass
+
+
+class ConstantLearningRateSchedule(LearningRateSchedule):
+    def __init__(self, value):
+        self.value = value
+
+    def get_learning_rate(self, epoch):
+        return self.value
+
+
+class StepLearningRateSchedule(LearningRateSchedule):
+    def __init__(self, initial, interval, factor):
+        self.initial = initial
+        self.interval = interval
+        self.factor = factor
+
+    def get_learning_rate(self, epoch):
+
+        return self.initial * (self.factor ** (epoch // self.interval))
+
+
+class WarmupLearningRateSchedule(LearningRateSchedule):
+    def __init__(self, initial, warmed_up, length):
+        self.initial = initial
+        self.warmed_up = warmed_up
+        self.length = length
+
+    def get_learning_rate(self, epoch):
+        if epoch > self.length:
+            return self.warmed_up
+        return self.initial + (self.warmed_up - self.initial) * epoch / self.length
+
+
+def get_learning_rate_schedules(specs):
+
+    schedule_specs = specs["LearningRateSchedule"]
+
+    schedules = []
+
+    for schedule_specs in schedule_specs:
+
+        if schedule_specs["Type"] == "Step":
+            schedules.append(
+                StepLearningRateSchedule(
+                    schedule_specs["Initial"],
+                    schedule_specs["Interval"],
+                    schedule_specs["Factor"],
+                )
+            )
+        elif schedule_specs["Type"] == "Warmup":
+            schedules.append(
+                WarmupLearningRateSchedule(
+                    schedule_specs["Initial"],
+                    schedule_specs["Final"],
+                    schedule_specs["Length"],
+                )
+            )
+        elif schedule_specs["Type"] == "Constant":
+            schedules.append(ConstantLearningRateSchedule(schedule_specs["Value"]))
+
+        else:
+            raise Exception(
+                'no known learning rate schedule of type "{}"'.format(
+                    schedule_specs["Type"]
+                )
+            )
+
+    return schedules
+
+
+def save_model(experiment_directory, filename, decoder, epoch):
+
+    model_params_dir = ws.get_model_params_dir(experiment_directory, True)
+
+    torch.save(
+        {"epoch": epoch, "model_state_dict": decoder.state_dict()},
+        os.path.join(model_params_dir, filename),
+    )
+
+
+def save_optimizer(experiment_directory, filename, optimizer, epoch):
+
+    optimizer_params_dir = ws.get_optimizer_params_dir(experiment_directory, True)
+
+    torch.save(
+        {"epoch": epoch, "optimizer_state_dict": optimizer.state_dict()},
+        os.path.join(optimizer_params_dir, filename),
+    )
+
+
+def load_optimizer(experiment_directory, filename, optimizer):
+
+    full_filename = os.path.join(
+        ws.get_optimizer_params_dir(experiment_directory), filename
+    )
+
+    if not os.path.isfile(full_filename):
+        raise Exception(
+            'optimizer state dict "{}" does not exist'.format(full_filename)
+        )
+
+    data = torch.load(full_filename)
+
+    optimizer.load_state_dict(data["optimizer_state_dict"])
+
+    return data["epoch"]
+
+
+def save_latent_vectors(experiment_directory, filename, latent_vec, epoch):
+
+    latent_codes_dir = ws.get_latent_codes_dir(experiment_directory, True)
+
+    all_latents = latent_vec.state_dict()
+
+    torch.save(
+        {"epoch": epoch, "latent_codes": all_latents},
+        os.path.join(latent_codes_dir, filename),
+    )
+
+
+# TODO: duplicated in workspace
+def load_latent_vectors(experiment_directory, filename, lat_vecs):
+
+    full_filename = os.path.join(
+        ws.get_latent_codes_dir(experiment_directory), filename
+    )
+
+    if not os.path.isfile(full_filename):
+        raise Exception('latent state file "{}" does not exist'.format(full_filename))
+
+    data = torch.load(full_filename)
+
+    if isinstance(data["latent_codes"], torch.Tensor):
+
+        # for backwards compatibility
+        if not lat_vecs.num_embeddings == data["latent_codes"].size()[0]:
+            raise Exception(
+                "num latent codes mismatched: {} vs {}".format(
+                    lat_vecs.num_embeddings, data["latent_codes"].size()[0]
+                )
+            )
+
+        if not lat_vecs.embedding_dim == data["latent_codes"].size()[2]:
+            raise Exception("latent code dimensionality mismatch")
+
+        for i, lat_vec in enumerate(data["latent_codes"]):
+            lat_vecs.weight.data[i, :] = lat_vec
+
+    else:
+        lat_vecs.load_state_dict(data["latent_codes"])
+
+    return data["epoch"]
+
+
+def save_logs(
+    experiment_directory,
+    loss_log,
+    lr_log,
+    timing_log,
+    lat_mag_log,
+    param_mag_log,
+    epoch,
+):
+
+    torch.save(
+        {
+            "epoch": epoch,
+            "loss": loss_log,
+            "learning_rate": lr_log,
+            "timing": timing_log,
+            "latent_magnitude": lat_mag_log,
+            "param_magnitude": param_mag_log,
+        },
+        os.path.join(experiment_directory, ws.logs_filename),
+    )
+
+
+def load_logs(experiment_directory):
+
+    full_filename = os.path.join(experiment_directory, ws.logs_filename)
+
+    if not os.path.isfile(full_filename):
+        raise Exception('log file "{}" does not exist'.format(full_filename))
+
+    data = torch.load(full_filename)
+
+    return (
+        data["loss"],
+        data["learning_rate"],
+        data["timing"],
+        data["latent_magnitude"],
+        data["param_magnitude"],
+        data["epoch"],
+    )
+
+
+def clip_logs(loss_log, lr_log, timing_log, lat_mag_log, param_mag_log, epoch):
+
+    iters_per_epoch = len(loss_log) // len(lr_log)
+
+    loss_log = loss_log[: (iters_per_epoch * epoch)]
+    lr_log = lr_log[:epoch]
+    timing_log = timing_log[:epoch]
+    lat_mag_log = lat_mag_log[:epoch]
+    for n in param_mag_log:
+        param_mag_log[n] = param_mag_log[n][:epoch]
+
+    return (loss_log, lr_log, timing_log, lat_mag_log, param_mag_log)
+
+
+def get_spec_with_default(specs, key, default):
+    try:
+        return specs[key]
+    except KeyError:
+        return default
+
+
+def get_mean_latent_vector_magnitude(latent_vectors):
+    return torch.mean(torch.norm(latent_vectors.weight.data.detach(), dim=1))
+
+
+def append_parameter_magnitudes(param_mag_log, model):
+    for name, param in model.named_parameters():
+        if len(name) > 7 and name[:7] == "module.":
+            name = name[7:]
+        if name not in param_mag_log.keys():
+            param_mag_log[name] = []
+        param_mag_log[name].append(param.data.norm().item())
+
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    arg_parser = argparse.ArgumentParser(description="Train a DeepSDF autodecoder")
+    arg_parser.add_argument(
+        "--experiment",
+        "-e",
+        dest="experiment_directory",
+        required=True,
+        help="The experiment directory. This directory should include "
+        + "experiment specifications in 'specs.json', and logging will be "
+        + "done in this directory as well.",
+    )
+    arg_parser.add_argument(
+        "--continue",
+        "-c",
+        dest="continue_from",
+        help="A snapshot to continue from. This can be 'latest' to continue"
+        + "from the latest running snapshot, or an integer corresponding to "
+        + "an epochal snapshot.",
+    )
+    arg_parser.add_argument(
+        "--batch_split",
+        dest="batch_split",
+        default=1,
+        help="This splits the batch into separate subbatches which are "
+        + "processed separately, with gradients accumulated across all "
+        + "subbatches. This allows for training with large effective batch "
+        + "sizes in memory constrained environments.",
+    )
+
+    deep_sdf.add_common_args(arg_parser)
+
+    args = arg_parser.parse_args()
+
+    deep_sdf.configure_logging(args)
+
+    experiment_directory = args.experiment_directory
+    continue_from = args.continue_from
+    batch_split = args.batch_split
+
+    specs = ws.load_experiment_specifications(experiment_directory)
+
+    arch = __import__("networks." + specs["NetworkArch"], fromlist=["Decoder"])
+    latent_size = specs["CodeLength"]
+
+    checkpoints = list(
+        range(
+            specs["SnapshotFrequency"],
+            specs["NumEpochs"] + 1,
+            specs["SnapshotFrequency"],
+        )
+    )
+
+    for checkpoint in specs["AdditionalSnapshots"]:
+        checkpoints.append(checkpoint)
+    checkpoints.sort()
+
+    grad_clip = get_spec_with_default(specs, "GradientClipNorm", None)
+    if grad_clip is not None:
+        logging.debug("clipping gradients to max norm {}".format(grad_clip))
+
+    
+    decoder = arch.Decoder(latent_size, **specs["NetworkSpecs"]).cuda()
+
+    
