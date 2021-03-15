7ef586b13c72c15d36495c073c0c682d4ebda745
trisct <29689546+trisct@users.noreply.github.com>
2021-03-15 16:47:31 +0800

    new file:   sync_to_szq.sh

---
+++ sync_to_szq.sh
@@ -0,0 +1,1 @@
+rsync --inplace --include="*shapnetv1*" --include="*shapenetv1*" --exclude="datasets_raw/" --exclude="datasets_processed/*/*" --exclude="bike_exp*/" --exclude="bin/" --exclude="build/" * szq_server_group:~/linsiyou/cvlab/DeepSDF/
